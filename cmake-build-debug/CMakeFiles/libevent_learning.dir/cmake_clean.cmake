597cd3d61bcb9f214b99cf2e4b0e6be543dce244
chenjingtao <chenjingtao@xiaomi.com>
2018-12-11 12:39:50 -0800

    add

---
+++ cmake-build-debug/CMakeFiles/libevent_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libevent_learning.dir/src/server.cc.o"
+  "../lib/libevent_learning.pdb"
+  "../lib/libevent_learning.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libevent_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
